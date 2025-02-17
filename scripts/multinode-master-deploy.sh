a398e05f958a781bf2d70b681a02d06c9c552ce0
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2025-02-17 18:47:48 +0100

    fix chmod script

---
+++ scripts/multinode-master-deploy.sh
@@ -0,0 +1,15 @@
+printf "\033[1;33m## START KUBEADM \032[0m\n"
+sudo kubeadm reset -f
+sudo kubeadm init --control-plane-endpoint=master-node --upload-certs --pod-network-cidr=10.244.0.0/16 | tee /tmp/kubeadm_init.log
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+printf "\033[1;33m## DEPLOY FLANNEL NETWORK \032[0m\n"
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+printf "\033[1;33m## note : the command below is requiered for worker nodes \033[0m\n"
+printf "\033[1;32m"
+tail -n 2 /tmp/kubeadm_init.log
+printf "\033[0m"
--- scripts/multinode-master.sh
+++ scripts/multinode-master.sh
@@ -28,18 +28,4 @@ EOF
 
 sudo systemctl daemon-reload && sudo systemctl restart kubelet
 
-printf "\033[1;33m## START KUBEADM \032[0m\n"
-sudo kubeadm reset -f
-sudo kubeadm init --control-plane-endpoint=master-node --upload-certs | tee /tmp/kubeadm_init.log
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-printf "\033[1;33m## DEPLOY FLANNEL NETWORK \032[0m\n"
-kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-
-printf "\033[1;33m## note : the command below is requiered for worker nodes \033[0m\n"
-printf "\033[1;32m"
-tail -n 2 /tmp/kubeadm_init.log
-printf "\033[0m"
+sh ./multinode-master-deploy.sh
