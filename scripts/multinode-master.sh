e26a8d9de07118ad0bd6c2aef479aec4d59a628e
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2025-02-20 10:05:34 +0100

    fix master script call

--- scripts/multinode-master.sh
+++ scripts/multinode-master.sh
@@ -6,9 +6,9 @@ printf "\033[1;31m## WARNING : you need to rename the hostname of this node with
 printf "\033[1;31m## WARNING : you need to modify the host file with command \033[1;33m'sudo nano /etc/hosts'\033[1;31m to add the \033[1;33m<hostname> <address ip> \033[0m(ex. 10.0.230.17 master-node)\033[31m line of this nodes and others nodes (master + workers). \n## You can add the other nodes address after \n## \n## to get ip address of your node, use \033[1;33m 'ip addr' \033[1;31m command. \033[0m\n"
 printf "\n\n"
 sleep 10
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-$SOURCE/mtnd-experience-requierements.sh
-$SOURCE/mtnd-requierements.sh
-$SOURCE/mtnd-docker.sh
-$SOURCE/mtnd-k8s.sh
-$SOURCE/multinode-resetcluster.sh
+echo "$PWD"
+/mtnd-experience-requierements.sh
+/mtnd-requierements.sh
+/mtnd-docker.sh
+/mtnd-k8s.sh
+/multinode-resetcluster.sh
