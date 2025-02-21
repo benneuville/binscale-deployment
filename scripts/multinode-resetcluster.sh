5afa0d9b4ef5023c875ce60606939ca57125ea9a
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2025-02-21 17:38:48 +0100

    experience 5

--- scripts/multinode-resetcluster.sh
+++ scripts/multinode-resetcluster.sh
@@ -8,7 +8,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
 
-printf "⚠️\033[1;33mnote : the command below is requiered for worker nodes \033[0m\n"
+printf "⚠️\033[1;33m Note : the command below is requiered for worker nodes \033[0m\n"
 printf "📍\033[1;32m "
 kubeadm token create --print-join-command
 printf "\033[0m"
