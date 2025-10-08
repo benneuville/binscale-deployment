3201664a22c3da3facf152787e4d5395913840ce
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2025-10-08 16:06:14 +0200

    sed + versionning fixed

--- scripts/resetCluster.sh
+++ scripts/resetCluster.sh
@@ -1,3 +1,3 @@
 minikube delete
-minikube start --cpus=18 --memory=18g --insecure-registry 134.59.129.189:5000 --kubernetes-version v1.34.0
+minikube start --cpus=18 --memory=18g --insecure-registry 134.59.129.189:5000 --kubernetes-version v1.28.0
 minikube mount python/input:/var/log/experiments/ &
