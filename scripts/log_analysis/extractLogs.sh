fbf17ef926d5c0323bcfa02a0719105835434da3
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2026-04-28 16:24:28 +0200

    deleting filebeat ignore not find

--- scripts/log_analysis/extractLogs.sh
+++ scripts/log_analysis/extractLogs.sh
@@ -1,5 +1,5 @@
 echo "Extracting consumer logs"
-jq 'select(.kubernetes.pod.name |startswith("latency")) |  .kubernetes.pod.uid + " - " + .kubernetes.container.name + " - " + .message'  $@  2>/dev/null | tr -d '"' > consumer_logs.txt &
+jq 'select(.kubernetes.labels.app |startswith("latency")) |  .kubernetes.pod.uid + " - " + .kubernetes.container.name + " - " + .message'  $@  2>/dev/null | tr -d '"' > consumer_logs.txt &
 echo "Extracting producer logs"
 jq 'select(.kubernetes.pod.name |startswith("workload")) | .message' $@  2>/dev/null | tr -d '"' > producer_logs.txt &
 echo "Extracting controller logs"
