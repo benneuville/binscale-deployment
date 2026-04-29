73487915ada822f012080dfb91db01337ef4d719
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2026-04-29 09:48:41 +0200

    filebeat diff file per pods fix

--- scripts/log_analysis/extractLogs.sh
+++ scripts/log_analysis/extractLogs.sh
@@ -1,8 +1,8 @@
 echo "Extracting consumer logs"
 jq 'select(.kubernetes.labels.app |startswith("latency")) |  .kubernetes.pod.uid + " - " + .kubernetes.container.name + " - " + .message'  $@  2>/dev/null | tr -d '"' > consumer_logs.txt &
 echo "Extracting producer logs"
-jq 'select(.kubernetes.pod.name |startswith("workload")) | .message' $@  2>/dev/null | tr -d '"' > producer_logs.txt &
+jq 'select(.kubernetes.labels.app |startswith("workload")) | .message' $@  2>/dev/null | tr -d '"' > producer_logs.txt &
 echo "Extracting controller logs"
-jq 'select(.kubernetes.pod.name |startswith("controllerand")) | .message' $@  2>/dev/null | tr -d '"' > controller_logs.txt &
+jq 'select(.kubernetes.labels.app |startswith("controllerandscaler")) | .message' $@  2>/dev/null | tr -d '"' > controller_logs.txt &
 
 wait
