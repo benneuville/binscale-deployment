d5b38fbe5988291ff663e498d40b40ec08d9ee9b
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2026-06-02 17:34:56 +0200

    persistence volume for postgres

--- scripts/mtnd-nfs-master.sh
+++ scripts/mtnd-nfs-master.sh
@@ -5,12 +5,24 @@ printf "💾\033[1;31m INSTALL & SETUP NFS SERVER \033[0m\n"
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y nfs-kernel-server
 
+# export logs
 sudo mkdir -p /export/logs
 
 sudo chown nobody:nogroup /export/logs
 
 if ! grep -qF "/export/logs" /etc/exports; then
     echo "/export/logs    *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+fi
+
+# export postgres
+
+sudo mkdir -p /export/postgres
+
+sudo chown nobody:nogroup /export/postgres
+sudo chmod 777 /export/postgres
+
+if ! grep -qF "/export/postgres" /etc/exports; then
+    echo "/export/postgres    *(rw,sync,no_subtree_check,no_root_squash)" | sudo tee -a /etc/exports
 fi
 
 sudo exportfs -ra
--- scripts/mtnd-nfs-worker.sh
+++ scripts/mtnd-nfs-worker.sh
@@ -5,3 +5,6 @@ sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y nfs-common 
 sudo mkdir -p /var/log/experiments
 sudo mount -t nfs master-node:/export/logs /var/log/experiments
+
+sudo mkdir -p /mnt/nfs/postgres
+sudo mount -t nfs master-node:/export/postgres /mnt/nfs/postgres
