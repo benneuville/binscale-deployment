285e0a1991959121d2b7b36fbea09c5a039995cf
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2026-06-05 16:29:31 +0200

    logs folder

--- scripts/multinode-complete-deployment.sh
+++ scripts/multinode-complete-deployment.sh
@@ -439,14 +439,14 @@ for file in $INPUT_GRAPH_FOLDER/*.bs.yaml; do
     printf "\r\033[38;5;36m ▣ Output directory created. [$DIR_OUTPUT_FINAL]\033[0m\n"
 
     scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -J "$SITE_NAME.g5k" "root@$master_node:/export/analyzer/*" "$DIR_OUTPUT_FINAL"
-    scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -J "$SITE_NAME.g5k" "root@$master_node:/export/logs/*" "$DIR_OUTPUT_FINAL"
+    scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -J "$SITE_NAME.g5k" "root@$master_node:/export/logs/*" "$DIR_OUTPUT_FINAL/logs/"
     printf "\033[38;5;36m ▣ Experience [$file] run completed. Logs retrieved in \033[0m[$DIR_OUTPUT_FINAL]\n"
     
     if [ "$is_analyze_mode" = true ]; then
         printf "\r\033[38;5;36m ▣ Analyze processed in parallel [$file_name]\033[0m\n"
         {
             cd $DIR_OUTPUT_FINAL || exit 1
-            "$SCRIPT_DIR/log_analysis/extractLogs.sh" filebeat* >/dev/null 2>&1
+            "$SCRIPT_DIR/log_analysis/extractLogs.sh" ./logs/filebeat* >/dev/null 2>&1
             python3 "$SCRIPT_DIR/log_analysis/mtnd-analysis.py" consumer_logs.txt controller_logs.txt &
             python3 "$SCRIPT_DIR/../experience/generator/graph_visualizor.py" $SCRIPT_DIR/../$file &
             wait
