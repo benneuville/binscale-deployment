6bc1423f77d50fa994ceb08ca4ae34b6dd38f1bc
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2026-07-29 13:40:30 +0200

    upgrade version calico

--- scripts/multinode-resetcluster.sh
+++ scripts/multinode-resetcluster.sh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.0/manifests/calico.yaml
 
 printf "⚠️\033[1;33m Note : the command below is requiered for worker nodes \033[0m\n"
 printf "📍\033[1;32m "
