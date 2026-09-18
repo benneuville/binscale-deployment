697116f8506e812265f4f3f9461d6bd922e0c0e5
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2026-09-18 16:22:52 +0200

    add e2e

--- scripts/log_analysis/extractLogs.sh
+++ scripts/log_analysis/extractLogs.sh
@@ -4,5 +4,7 @@ echo "Extracting producer logs"
 jq 'select(.kubernetes.labels.app |startswith("workload")) | .message' $@  2>/dev/null | tr -d '"' > producer_logs.txt &
 echo "Extracting controller logs"
 jq 'select(.kubernetes.labels.app |startswith("controllerandscaler")) | .message' $@  2>/dev/null | tr -d '"' > controller_logs.txt &
+echo "Extracting e2e logs"
+jq 'select(.kubernetes.labels.app |startswith("e2e-analyzer")) | .message' $@  2>/dev/null | tr -d '"' > e2e_logs.txt &
 
 wait
